f74d54abfc33010086ab39a99acec0c8ac59fe10
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2024-06-12 14:12:27 +0200

    Update zad5.c

--- final/zad5.c
+++ final/zad5.c
@@ -1,161 +1,166 @@
-// CONFIGURATION BITS
-#pragma config POSCMOD = NONE             // Primary Oscillator Select (primary oscillator disabled)
-#pragma config OSCIOFNC = OFF             // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
-#pragma config FCKSM = CSDCMD             // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
-#pragma config FNOSC = FRC                // Oscillator Select (Fast RC Oscillator without Postscaler)
-#pragma config IESO = OFF                 // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
-#pragma config WDTPS = PS32768            // Watchdog Timer Postscaler (1:32,768)
-#pragma config FWPSA = PR128              // WDT Prescaler (1:128)
-#pragma config WINDIS = ON                // Watchdog Timer Window Mode disabled
-#pragma config FWDTEN = OFF               // Watchdog Timer disabled
-#pragma config ICS = PGx2                 // Emulator/debugger uses EMUC2/EMUD2
-#pragma config GWRP = OFF                 // Writes to program memory allowed
-#pragma config GCP = OFF                  // Code protection is disabled
-#pragma config JTAGEN = OFF               // JTAG port is disabled
+#pragma config POSCMOD = NONE
+#pragma config OSCIOFNC = OFF
+#pragma config FCKSM = CSDCMD
+#pragma config FNOSC = FRC
+#pragma config IESO = OFF
+#pragma config WDTPS = PS32768
+#pragma config FWPSA = PR128
+#pragma config WINDIS = ON
+#pragma config FWDTEN = OFF
+#pragma config ICS = PGx2
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
 
 #include "xc.h"
 #include <libpic30.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-// DEFINICJE MAKRO
-#define FCY         4000000UL   // Częstotliwość pracy oscylatora
-#define LCD_E       LATDbits.LATD4  
+#define FCY         4000000UL
+#define LCD_ENABLE  LATDbits.LATD4  
 #define LCD_RW      LATDbits.LATD5
 #define LCD_RS      LATBbits.LATB15
 #define LCD_DATA    LATE
-#define LCD_CLEAR       0x01
-#define LCD_HOME        0x02
-#define LCD_ON          0x0C
-#define LCD_OFF         0x08
-#define LCD_CONFIG      0x38
-#define LCD_CURSOR      0x80
-#define LINE1           0x00
-#define LINE2           0x40
+#define LCD_CMD_CLEAR   0x01
+#define LCD_CMD_HOME    0x02
+#define LCD_CMD_ON      0x0C
+#define LCD_CMD_OFF     0x08
+#define LCD_CMD_CONFIG  0x38
+#define LCD_CMD_CURSOR  0x80
+#define LINIA_PIERWSZA  0x00
+#define LINIA_DRUGA     0x40
+#define LCD_CUSTOM_CHAR 0x40
+#define LCD_SHIFT_RIGHT 0x1D
 
-// Funckje delay
-void __delay_us(unsigned long us){
+void delay_microseconds(unsigned long us){
     __delay32(us * FCY / 1000000);
 }
 
-void __delay_ms(unsigned long ms){
+void delay_milliseconds(unsigned long ms){
     __delay32(ms * FCY / 1000);
 }
 
-// Funkcje LCD
-void LCD_sendCommand(unsigned char command){
+void LCD_sendCmd(unsigned char cmd){
     LCD_RW = 0;     
     LCD_RS = 0;     
-    LCD_E = 1;      
-    LCD_DATA = command;
-    __delay_us(50);
-    LCD_E = 0;
+    LCD_ENABLE = 1;      
+    LCD_DATA = cmd;
+    delay_microseconds(50);
+    LCD_ENABLE = 0;
 }
 
-void LCD_sendData(unsigned char data){
+void LCD_sendDataByte(unsigned char data){
     LCD_RW = 0;
     LCD_RS = 1;     
-    LCD_E = 1;
+    LCD_ENABLE = 1;
     LCD_DATA = data;
-    __delay_us(50);
-    LCD_E = 0;
+    delay_microseconds(50);
+    LCD_ENABLE = 0;
 }
 
-void LCD_print(unsigned char* string){
+void LCD_displayString(unsigned char* string){
     while(*string){
-        LCD_sendData(*string++);
+        LCD_sendDataByte(*string++);
     }
 }
 
-void LCD_setCursor(unsigned char row, unsigned char col){
+void LCD_setPosition(unsigned char row, unsigned char col){
     unsigned char address;
     if (row == 1){
-        address = LCD_CURSOR + LINE1 + col;
+        address = LCD_CMD_CURSOR + LINIA_PIERWSZA + col;
     }
-    if (row == 2){
-        address = LCD_CURSOR + LINE2 + col;
+    if(row == 2){
+        address = LCD_CMD_CURSOR + LINIA_DRUGA + col;
     }
-    LCD_sendCommand(address);
+    LCD_sendCmd(address);
 }
 
-void LCD_init(){
-    __delay_ms(20);
-    LCD_sendCommand(LCD_CONFIG);
-    __delay_us(50);
-    LCD_sendCommand(LCD_ON);
-    __delay_us(50);
-    LCD_sendCommand(LCD_CLEAR);
-    __delay_ms(2);
+void LCD_initialize(){
+    delay_milliseconds(20);
+    LCD_sendCmd(LCD_CMD_CONFIG);
+    delay_microseconds(50);
+    LCD_sendCmd(LCD_CMD_ON);
+    delay_microseconds(50);
+    LCD_sendCmd(LCD_CMD_CLEAR);
+    delay_milliseconds(2);
 }
 
-void display_time(unsigned int time, int cursor){
-    LCD_setCursor(cursor, 0);
-    LCD_print("Time: ");
-    LCD_sendData('0' + time / 60);
-    LCD_sendData(':');
-    LCD_sendData('0' + (time % 60) / 10);
-    LCD_sendData('0' + time % 10);
-}
+void showTime(unsigned int time, unsigned char row){
+    unsigned char minutes = time / 60;
+    unsigned char seconds = time % 60;
 
+    LCD_setPosition(row, 0);
+    if (row == 1) {
+        LCD_displayString("Player 1: ");
+    } else {
+        LCD_displayString("Player 2: ");
+    }
+    LCD_sendDataByte('0' + minutes / 10);
+    LCD_sendDataByte('0' + minutes % 10);
+    LCD_sendDataByte(':');
+    LCD_sendDataByte('0' + seconds / 10);
+    LCD_sendDataByte('0' + seconds % 10);
+}
 
 int main(void) {
-    TRISB = 0x7FFF;     // Ustawienie rejestrow kierunku
+    TRISB = 0x7FFF;
     TRISD = 0xFFE7;
     TRISE = 0x0000;
-    TRISA = 0x0000;
+    TRISA = 0xFFFF;
+    
+    LCD_initialize();
+    
+    unsigned int timePlayer1 = 3;
+    unsigned int timePlayer2 = 300;
+    bool activePlayer1 = true;
+    bool running = false;
+    
+    unsigned char buttonState6 = 0, prevButtonState6 = 0;
+    unsigned char buttonState7 = 0, prevButtonState7 = 0;
+    
+    unsigned int loopCounter = 0;
+    const unsigned int loopsPerSecond = 100 / 1.3;
 
-    LCD_init();         // Inicjalizacja wyświetlacza
-
-    unsigned int time_player1 = 300; // 5 minut w sekundach
-    unsigned int time_player2 = 300; // 5 minut w sekundach
-    bool player1_turn = true; // flagi na tury
-    bool player2_turn = false;
-    bool game_over = false; // flaga konca gry
-	
     while(1) {
-        if(game_over) {
-            LCD_setCursor(1, 0);
-            LCD_print("Game Over.");
-            while(1); // Zatrzymaj program
-        }
-
-        if(PORTDbits.RD6==0){  // Przycisk gracza 1
-            player1_turn = false;
-            player2_turn = true;
-            
-        }
-
-        if(PORTDbits.RD7==0){  // Przycisk gracza 2
-            player2_turn = false;
-            player1_turn = true;
-        }
-
-        if(player1_turn) {
-            __delay_ms(1000); // Odliczanie co sekundę
-            time_player1--;
-        }
-
-        if(player2_turn) {
-            __delay_ms(1000); // Odliczanie co sekundę
-            time_player2--;
-        }
-
-        display_time(time_player1,1);
-        display_time(time_player2,2);
+        buttonState6 = PORTDbits.RD6;
+        buttonState7 = PORTDbits.RD7;
         
-        // Koniec czasu
-        if(time_player1 == 0) {
-            LCD_setCursor(2, 0);
-            LCD_print("Player 1 loses");
-            game_over = true;
+        if(buttonState6 && !prevButtonState6){
+            activePlayer1 = !activePlayer1;
+            running = true;
         }
-
-        if(time_player2 == 0) {
-            LCD_setCursor(2, 0);
-            LCD_print("Player 2 loses");
-            game_over = true;
+        
+        if(buttonState7 && !prevButtonState7){
+            timePlayer1 = 300;
+            timePlayer2 = 300;
+            running = false;
+        }
+        
+        prevButtonState6 = buttonState6;      
+        prevButtonState7 = buttonState7;
+        
+        showTime(timePlayer1, 1);
+        showTime(timePlayer2, 2);
+        
+        if(running){
+            delay_milliseconds(10);
+            loopCounter++;
+            if(loopCounter >= loopsPerSecond){
+                loopCounter = 0;
+                if(activePlayer1){
+                    if(timePlayer1 > 0) timePlayer1--;
+                } else {
+                    if(timePlayer2 > 0) timePlayer2--;
+                }
+            }
+        } else {
+            delay_milliseconds(10);
+        }
+        
+        if(timePlayer1 == 0 || timePlayer2 == 0){
+            running = false;
         }
     }
-
+    
     return 0;
 }
